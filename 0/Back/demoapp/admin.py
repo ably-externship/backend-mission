91b8f83eccdadc4be3967bd661d63757ad87e501
jhs512 <jangka512@gmail.com>
2022-01-05 10:27:22 +0900

    Delete 0 directory

--- 0/Back/demoapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Todo
-
-# Register your models here.
-@admin.register(Todo)
-class TodoAdmin(admin.ModelAdmin):
-    pass
--- 0/Back/demoapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TodoGeneralView.as_view()),
-    path('<int:pk>/', views.TodoSepcView.as_view()),
-]
