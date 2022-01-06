1f76a516e908b71e0c63785074e7184b82df8900
SangRakKun <leesangrak0307@gmail.com>
2022-01-06 09:41:15 +0900

    feature : connect MySQL

--- missions/week 1/back/back/settings.py
+++ missions/week 1/back/back/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# # MySQL 연동
 # DATABASES = {
-# 	'default': {
-#     	'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mall',
-#         'USER': 'root',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#      }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+# MySQL 연동
+DATABASES = {
+	'default': {
+    	'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mall',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
