acd26a109fe40a9099d8eb7f2178df0db869003b
KyeongHwanMin <h45652@naver.com>
2022-01-06 20:35:33 +0900

    refactor: 비밀번호 초기화 이메일로 전송하기

--- missions/week 1/back/backend_mission/accounts/urls.py
+++ missions/week 1/back/backend_mission/accounts/urls.py
@@ -2,9 +2,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, reverse_lazy
 from . import views
 from .views import AccountDetailView, AccountUpdateView, AccountCreateView, AccountDeleteView
-from django.contrib.auth import views as auth_views
 
-app_name = "accounts"
+
+app_name = 'accounts'
 
 urlpatterns = [
 
@@ -15,10 +15,6 @@ urlpatterns = [
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     # path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
--- missions/week 1/back/backend_mission/base/urls.py
+++ missions/week 1/back/backend_mission/base/urls.py
@@ -2,11 +2,18 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name='base'),
     path('accounts/', include('accounts.urls')),
     path('product/', include('product.urls')),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
 ]
