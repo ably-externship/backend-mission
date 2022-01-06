4901e463befb048ea1729e2eca656c044d2eb4b4
KyeongHwanMin <h45652@naver.com>
2022-01-06 19:57:14 +0900

    feat: 비밀번호 초기화 진행중

--- missions/week 1/back/backend_mission/accounts/urls.py
+++ missions/week 1/back/backend_mission/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from .views import AccountDetailView, AccountUpdateView, AccountCreateView, AccountDeleteView
+from django.contrib.auth import views as auth_views
 
 app_name = "accounts"
 
@@ -13,5 +14,17 @@ urlpatterns = [
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
+    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
+    # path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # path('send_email/', views.send_email, name='send_email'),
 
 ]
