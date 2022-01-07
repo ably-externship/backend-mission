f5db78d7015fbdcb629fe29f99b87c1e33c24b10
ulr0 <ulr0xb@gmail.com>
2022-01-07 22:18:44 +0900

    add login 기능 - login_views.py 생성 및 작성 - urls.py 라우팅 추가

--- missions/week 1/back/ably/accounts/urls.py
+++ missions/week 1/back/ably/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from accounts.signup_views import SignUpView
+from accounts.login_views import LogInView
 
 urlpatterns = [
     path('/signup', SignUpView.as_view()),
+    path('/login', LogInView.as_view()),
 ]
