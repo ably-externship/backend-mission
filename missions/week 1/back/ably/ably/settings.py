f26c7fb0434e41308eb0ff4c264dcaa57914c4fa
ulr0 <ulr0xb@gmail.com>
2022-01-07 15:51:50 +0900

    add migrations - 앱 생성 - models.py 작성 - migrate 완료

--- missions/week 1/back/ably/ably/settings.py
+++ missions/week 1/back/ably/ably/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'accounts',
+    'products',
+    'qnas'
 ]
 
 MIDDLEWARE = [
