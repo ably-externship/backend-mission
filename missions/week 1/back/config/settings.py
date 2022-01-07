9e70af7bed05047e50e40632972d9b53ed7b83c6
KyeongHwanMin <h45652@naver.com>
2022-01-08 03:03:09 +0900

    feat: DB 셋업 및 장고와 연동

--- missions/week 1/back/config/settings.py
+++ missions/week 1/back/config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mutbly_db',
+        'USER': 'root',
+        'PASSWORD': 'rootpass',
+        'HOST': '127.0.0.1',
+        'PORT': '63306',
     }
 }
 
