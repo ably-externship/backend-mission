f5eec584dcedc5d7be83c60aa1096e8eb1dfc0a9
ulr0 <ulr0xb@gmail.com>
2022-01-09 00:50:23 +0900

    add question post - question_views.py 생성 및 작성 - urls.py 수정 - validator 추가

--- missions/week 1/back/ably/ably/urls.py
+++ missions/week 1/back/ably/ably/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts', include('accounts.urls')),
     path('products', include('products.urls')),
+    path('qnas', include('qnas.urls')),
 ]
--- missions/week 1/back/ably/core/decorators.py
+++ missions/week 1/back/ably/core/decorators.py
@@ -6,21 +6,24 @@ from accounts.models import User
 from my_settings import SECRET_KEY, ALGORITHM
 
 def login_required(func):
-    def wrapper(self, request, *arg, **kargs):
+    def wrapper(self, request, *arg, **kwargs):
         try:
             access_token = request.headers.get('Authorization', None)
-            payload = jwt.decode(access_token, SECRET_KEY, algorithms = ALGORITHM)
+            if not access_token:
+                return JsonResponse({'message' : 'Unauthorized Access'}, status=401)
+
+            payload = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
             user = User.objects.get(id = payload['id'])
 
             if user.userinfo.is_deleted:
                 return JsonResponse({'message' : 'Invalid User'}, status = 401)
             
             request.user = user
-
-        except jwt.exceptions.DecodeError:
+            
+        except jwt.DecodeError:
             JsonResponse({'message' : 'Unauthorized Token'}, status = 401)
         except User.DoesNotExist:
             return JsonResponse({'message' : 'Invalid User'}, status = 401)
-
-        return func(self, request, *arg, **kargs)
+        
+        return func(self, request, *arg, **kwargs)
     return wrapper
