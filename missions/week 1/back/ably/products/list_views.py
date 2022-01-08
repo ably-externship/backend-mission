34a333012b800ee3e79098ac96e1868d99bd4819
ulr0 <ulr0xb@gmail.com>
2022-01-08 21:27:33 +0900

    add product list 기능 - list_views.py 생성 및 작성 - urls.py 라우팅 추가

---
+++ missions/week 1/back/ably/products/list_views.py
@@ -0,0 +1,34 @@
+from django.http import JsonResponse
+from django.views import View
+
+from products.models import ProductList
+
+class ProductListView(View):
+    def get(self, request):
+        PAGINATION_LIMIT = 6
+        
+        search_word = request.GET.get('search_word', '')
+        page = int(request.GET.get('page', 1))
+        offset = (page - 1) * PAGINATION_LIMIT
+        limit = offset + PAGINATION_LIMIT
+
+        products = ProductList.objects.filter(product_name__icontains = search_word)
+
+        product_lists = [
+            {
+                'id' : product.product_id,
+                'product_name' : product.product_name,
+                'price' : product.price,
+                'discount_price' : product.discount_price,
+                'main_image_url' : product.main_image_url,
+                'seller_name' : product.seller_name
+            }
+            for product in products[offset:limit]
+        ]
+        
+        return JsonResponse(
+            {
+                'product_lists' : product_lists, 
+                'product_counts' : products.count()
+                }, 
+            status = 200)
