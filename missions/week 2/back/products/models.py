42b1fd060cb4270e65d21fe764dcd50f35ed1657
junyoung <jy940608@naver.com>
2022-01-14 13:52:44 +0900

    UPDATE: 제품 모델링 수정

--- missions/week 2/back/products/models.py
+++ missions/week 2/back/products/models.py
@@ -5,37 +5,31 @@ from core import models as core_models
 
 
 class Product(core_models.DateTimeModel):
-    COLOR_CHOICES = (
-        ('white', '흰색'),
-        ('black', '검정색'),
-        ('red', '빨간색'),
-        ('green', '초록색'),
-        ('blue', '파란색'),
-    )
-    SIZE_CHOICES = (
-        ('s', 'SMALL'),
-        ('m', 'MEDIUM'),
-        ('l', 'LARGE'),
-    )
-    CATEGORY_CHOICES = (
-        ('neat', '니트'),
-        ('hood', '후드'),
-        ('mtm', '맨투맨'),
-        ('shirt', '셔츠'),
-    )
     market = models.ForeignKey('markets.Market', on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=30)
     price = models.PositiveIntegerField(default=0)
-    color = models.CharField(max_length=10, choices=COLOR_CHOICES)
-    size = models.CharField(max_length=1, choices=SIZE_CHOICES)
-    category = models.CharField(max_length=5, choices=CATEGORY_CHOICES)
-    stock = models.PositiveIntegerField(default=1)  # 재고
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    is_delete = models.BooleanField(default=False)
+    is_sold_out = models.BooleanField(default=False)
+    is_hidden = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("products:detail", kwargs={"pk": self.pk})
+
+
+class ProductOption(core_models.DateTimeModel):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    color = models.CharField(max_length=10)
+    size = models.CharField(max_length=10)
+    stock = models.PositiveIntegerField(default=1)  # 재고
+    is_sold_out = models.BooleanField(default=False)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
 
 
 class Question(core_models.DateTimeModel):
