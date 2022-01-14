d64848d052607619922712995fbfbd773bc892b9
NQ-OO <lol1219@snu.ac.kr>
2022-01-15 02:57:12 +0900

    [chore] Add bootstrap icons

---
+++ missions/week 2/back/cart/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from mutbly.models import Item, Quantity
+# Create your models here.
+
+
--- missions/week 2/back/mutbly/settings.py
+++ missions/week 2/back/mutbly/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mutbly',
     'accounts',
+    'cart', 
 ]
 
 MIDDLEWARE = [
