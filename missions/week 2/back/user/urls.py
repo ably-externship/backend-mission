9a1030e932f667380a018c3057e4c6788a1ead67
tcha <hataeck@gmail.com>
2022-01-16 17:54:32 +0900

    implement cart

--- missions/week 2/back/user/urls.py
+++ missions/week 2/back/user/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = "user"
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-
+    path('users/login/kakao', views.KakaoSignInView.as_view(), name='login_kakao'),
+    path('users/login/kakao/callback',
+         views.KakaoSignInCallBackView.as_view(), name='login_kakao_callback'),
 ]
 
 
--- missions/week 2/back/user/views.py
+++ missions/week 2/back/user/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render, redirect
+from lib2to3.pgen2 import token
 from django.contrib.auth import authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from user.forms import UserForm
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+from user.forms import UserForm
+
+from cart.models import Cart
+from .models import User
+
+
+import os
+import requests
+import json
+
+from project.settings import BASE_DIR
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
 
 def register(request):
@@ -37,3 +56,56 @@ def login(request):
         form = AuthenticationForm()
 
     return render(request, 'user/login.html', {'form': form})
+
+
+class KakaoSignInView(View):
+
+    def get(self, request):
+        api_key = secrets["REST_API_KEY"]
+        redirect_uri = 'http://127.0.0.1:8000/users/login/kakao/callback'
+        kakao_auth_api = 'https://kauth.kakao.com/oauth/authorize?response_type=code'
+
+        return redirect(
+            f'{kakao_auth_api}&client_id={api_key}&redirect_uri={redirect_uri}'
+        )
+
+
+class KakaoSignInCallBackView(View):
+    def get(self, request):
+        auth_code = request.GET.get('code')
+        client_id = secrets["REST_API_KEY"]
+        kakao_token_api = 'https://kauth.kakao.com/oauth/token'
+        data = {
+            'grant_type': 'authorization_code',
+            'client_id': client_id,
+            'redirect_uri': 'http://127.0.0.1:8000/users/login/kakao/callback',
+            'code': auth_code,
+            'client_secret': secrets["CLIENT_SECRET"]
+        }
+
+        token_response = requests.post(kakao_token_api, data=data)
+
+        access_token = token_response.json().get('access_token')
+
+        user_info_response = requests.get(
+            'https://kapi.kakao.com/v2/user/me', headers={"Authorization": f'Bearer ${access_token}'})
+
+        user_info_response = user_info_response.json()
+
+        kakao_account = user_info_response.get("kakao_account")
+        profile = kakao_account.get("profile")
+
+        nickname = profile.get("nickname", None)
+        email = kakao_account.get("email", None)
+
+        user = User.objects.get_or_none(email=email)
+        if user is None:
+            user = User.objects.create_user(username=nickname, email=email)
+            user.set_unusable_password()
+            user.save()
+            cart = Cart.objects.create(user=user)
+            cart.save()
+
+        auth_login(request, user)
+
+        return redirect('product:index')
