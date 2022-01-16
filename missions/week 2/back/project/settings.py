ce7a0fa7c2b4d97873aeec6218e48175646add52
tcha <hataeck@gmail.com>
2022-01-16 19:41:16 +0900

    edit allowedhosts

--- missions/week 2/back/project/settings.py
+++ missions/week 2/back/project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = secrets["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
