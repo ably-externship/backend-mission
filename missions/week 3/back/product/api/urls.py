b3933a01f1ecb997caa2b2f769b0cd23a70b852a
KyeongHwanMin <h45652@naver.com>
2022-01-20 00:01:47 +0900

    Product API 구현

--- missions/week 3/back/product/api/urls.py
+++ missions/week 3/back/product/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from product.api.views import product_create_or_list, product_detail
 
-from product.api.views import ProductViewSet
+# from product.api.views import ProductViewSet
 
 # TODO: SimpleRouter vs DefaultRouter 차이점
-router = SimpleRouter()
-router.register('', ProductViewSet)
+# router = SimpleRouter()
+# router.register('', ProductViewSet)
 
 
 urlpatterns = [
@@ -15,5 +16,22 @@ urlpatterns = [
     # DELETE /api/products/cart/:cart_item_id/
     # path('cart/:cart_item_id/', cart_delete_view, name='cart_delete'),
 
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+
+    # REST 개념
+
+    # Rest 에서는 리소스 정의,
+    # 생성 POST /{리소스}
+    #
+
+    # 상품 추가 : POST /api/products/
+    # 상품 목록 조회 : GET /api/products/
+    path('', product_create_or_list),
+
+    # 상품 상세 조회 : GET /api/products/<int:pk>/
+    # 상품 수정 : PUT /api/products/products/<int:pk>/
+    # 상품 삭제 : DELETE /api/products/products/<int:pk>/
+    path('<int:pk>/', product_detail),
+
+
 ]
--- missions/week 3/back/product/api/views.py
+++ missions/week 3/back/product/api/views.py
@@ -1,42 +1,60 @@
+
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+
 
 from product.api.serializers import ProductSerializer
-from product.models import Product, CartItem
+from product.models import Product
 
 
-class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
-    
+
+def list_product():
+    serializer = ProductSerializer(Product.objects.all(), many=True)
+    return Response(serializer.data)
 
 
-# TODO: Django 의 Request 객체 vs DRF 의 Request 객체
-# django.core.handlers.wsgi.WSGIRequest
-# rest_framework.request.Request
+def retrieve_product(product):
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
-# @api_view(['POST'])
-# def change_quantity_api_view(request):
-#
-#     cart_item_id = request.data.get('id')
-#     quantity = request.data.get('quantity')
-#
-#     cart_item = CartItem.objects.get(user=request.user, id=cart_item_id)
-#     cart_item.quantity = quantity
-#     cart_item.save()
-#
-#     return Response(status=status.HTTP_200_OK)
 
-#
-# def cart_delete_view(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         print(data)
-#
-#         cart_item = CartItem.objects.get(user=request.user, id=data.get('id'))
-#         cart_item.delete()
-#
-#     return JsonResponse(data)
+def update_product(product, request):
+    serializer = ProductSerializer(product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def delete_product(product):
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def product_create_or_list(request):
+    if request.method == 'GET':
+        return list_product()
+    else:
+        return create_product(request)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        return retrieve_product(product)
+    elif request.method == 'PUT':
+        return update_product(product, request)
+    else:
+        return delete_product(product)
