32e4e30a495b465954b7ce3481a3972832bff96c
Kyeongjun <v49011591@gmail.com>
2022-01-22 20:51:10 +0900

    Django credential 설정 추가

--- missions/week 3/back/base/settings/local.py
+++ missions/week 3/back/base/settings/local.py
@@ -36,4 +36,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
+CORS_ALLOW_CREDENTIALS = True
 APPEND_SLASH = False
--- missions/week 3/back/product/api/serializers.py
+++ missions/week 3/back/product/api/serializers.py
@@ -1,27 +1,21 @@
 from rest_framework import serializers
 
-from market.models import Market
 from product.models import Product
-from rest_framework import exceptions
-from common.exception.ErrorMessage import ErrorMessage
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('market_pk', 'name', 'price', 'sold_out_yn', 'create_date')
-
-
-
+        fields = ('id', 'market_pk', 'name', 'price', 'sold_out_yn', 'create_date')
 
 
 class ProductPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('market_pk', 'category_fk','name', 'price', 'descriptions')
+        fields = ('market_pk', 'category_fk', 'name', 'price', 'descriptions')
+
 
 class ProductPutSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'market_pk', 'category_fk','name', 'price', 'descriptions')
-
+        fields = ('id', 'market_pk', 'category_fk', 'name', 'price', 'descriptions')
