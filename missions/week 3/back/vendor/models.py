b3020f70a91f01d52ff0fb8035b1e9e1ad33c786
tcha <hataeck@gmail.com>
2022-01-23 12:33:03 +0900

    Update checklist.md

--- missions/week 3/back/vendor/models.py
+++ missions/week 3/back/vendor/models.py
@@ -4,8 +4,6 @@ from django.db.models.fields import CharField
 
 class Vendor(models.Model):
     name = models.CharField(max_length=128)
-    product = models.ForeignKey(
-        'product.Product', on_delete=models.CASCADE, verbose_name="상품")
 
     def __str__(self):
-        return '{} {}'.format(self.name)
+        return '{}'.format(self.name)
