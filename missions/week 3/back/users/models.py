4862e3f4234976413de99fdfd54332e873726a1f
Kyeongjun <v49011591@gmail.com>
2022-02-02 22:56:39 +0900

    4주차 JWT Refresh Token 확인 후 재발급 처리 추가

--- missions/week 3/back/users/models.py
+++ missions/week 3/back/users/models.py
@@ -2,11 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from market.models import Market
+
 
 class User(AbstractUser):
     first_name = None
     last_name = None
     name = models.CharField(max_length=10, default='')
+    market_yn = models.BooleanField(default=False)
+    market_id = models.OneToOneField(Market, on_delete=models.CASCADE, null=True, blank=True)
     pass
 
 
@@ -15,5 +19,5 @@ class User(AbstractUser):
 
 class RefreshStorage(models.Model):
     hash_value = models.CharField(max_length=255)
-    refresh_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=500)
     user_fk = models.ForeignKey(User, on_delete=models.CASCADE, db_column='user_fk', default=1)
