079ca5f5186b2a24ba6147a28db8def4cfbf07f4
ulr0 <ulr0xb@gmail.com>
2022-02-05 22:46:12 +0900

    delete week3 files

--- missions/week 3/back/core/const.py
+++
@@ -1,7 +0,0 @@
-# account type
-MASTER_ACCOUNT_TYPE = 1
-USER_ACCOUNT_TYPE = 2
-SELLER_ACCOUNT_TYPE = 3
-
-# social_type
-KAKAO_SOCIAL_TYPE = 1
--- missions/week 3/back/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
-
-    class Meta:
-        abstract = True
