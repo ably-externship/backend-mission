d0b5cc6d6a6d6a50f48c4cf4afa33a1cd1f65913
dhleeone <dhleeone@gmail.com>
2022-02-06 15:14:04 +0900

    4주차

--- missions/week 4/front/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
