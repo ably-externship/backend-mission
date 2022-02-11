d1bfd1d97a40f8c344ce759a7c735a23726f15f5
wlsgh7608 <wlsgh7608@naver.com>
2022-02-11 14:24:16 +0900

    [add] 상품 상세 팝업창

--- missions/week 4/back/mbly/product/tests.py
+++ missions/week 4/back/mbly/product/tests.py
@@ -1,3 +1,17 @@
+from pydoc import cli
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class ProductTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username = "test1",password = "test1")
+        self.user2 = User.objects.create(username = "test2",password = "test2")
+    
+    def get_client(self):
+        client = APIClient()
+        client.login(username = self.user.username,password="test1")
+        return client
