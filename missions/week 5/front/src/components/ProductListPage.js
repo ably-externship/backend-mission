268ee4bef7a88b646dcf42f33633b50a66a2d668
ulr0 <ulr0xb@gmail.com>
2022-02-13 20:32:59 +0900

    상품 검색 기능 추가

--- missions/week 5/front/src/components/ProductListPage.js
+++ missions/week 5/front/src/components/ProductListPage.js
@@ -8,10 +8,27 @@ function ProductList (){
     const [products, setProducts] = useState([]);
     const [pages, setPages] = useState();
     const [currentPage, setCurrentPage] = useState(1);
-    const limit = 9;
+    const limit = 8;
+
+    const [search, setSearch] = useState('');
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    }
+    const onSubmitSearch = (e) => {
+        e.preventDefault();
+        setCurrentPage(1);
+
+        axios.get("http://localhost:8000/products/list", { params : { page : currentPage, search_word : search } })
+        .then((response)=>{
+            const productList = response.data.product_lists;
+            setProducts(productList);
+            const productCounts = response.data.product_counts;
+            setPages(Math.ceil(productCounts/limit));
+        })
+    }
 
     useEffect(()=>{
-        axios.get("http://localhost:8000/products/list", { params : { page : currentPage } })
+        axios.get("http://localhost:8000/products/list", { params : { page : currentPage, search_word : search } })
         .then((response)=>{
             const productList = response.data.product_lists;
             setProducts(productList);
@@ -21,12 +38,19 @@ function ProductList (){
     }, [currentPage]);
 
     return(
+        <>
+        
         <div className="container">
             <div className="row">
+            <div className="inputContainer">
+                <form onSubmit={onSubmitSearch}>
+                    <input placeholder="Search..." onChange={onChangeSearch}/>
+                </form>
+            </div>
                 {
                     products.map((a, i)=>{
                         return (
-                            <div key = {i} className="col-4">
+                            <div key = {i} className="col-3">
                                 <img src={a.main_image_url} />
                                 <h6>{a.product_name}</h6>
                                 <p>{Number(a.price)}원</p>
@@ -37,6 +61,7 @@ function ProductList (){
             </div>
             <PaginationBtns pages={pages} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
         </div>
+        </>
     )
 }
 
